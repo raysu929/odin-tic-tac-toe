7f154926949f1d9fca45f674ee0c2721b908acb0
Raeesah <raeesahmoola529@gmail.com>
2025-09-03 09:54:10 +0200

    Add switch players function and ternary operator to switch between players.

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const Gameboard = {
-board: [],
+board: ['', '', '',
+  '', '', '',
+  '', '', ''],
 }
 
 const players = {
-
+player1: 'X',
+player2: 'O',
+currentTurn: 'X'
 }
 
-const gameControl = {
-  
-}
+const gameControl = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 
 const container = document.querySelector(".container");
 const turn = document.querySelector(".turn");
@@ -22,7 +36,17 @@ for (let row = 0; row < 3; row++){
     button.classList.add("btn");
     button.addEventListener("click", () => {
 console.log(`button clicked at row ${row}, col ${col}`);
+
+function switchPlayers(){
+   players.currentTurn = players.currentTurn === players.player1 ? players.player2 : players.player1;
+      console.log(players.currentTurn);
+
+}
+  button.innerText = players.currentTurn;
+  switchPlayers();
+
 });
 board.appendChild(button);
-  }
 }
+}
+
